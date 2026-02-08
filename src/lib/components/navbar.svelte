982c23518ca4f04a6d1261faab6782285222844d
haider-9 <haiderahmad342@gmail.com>
2026-02-08 23:53:50 +0500

    first commit

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import { Bell, MessageCircle, Search, type Icon as typeoficon } from '@lucide/svelte';
+	import { Button } from './ui/button';
+	import * as DropdownMenu from './ui/dropdown-menu';
+	import * as Empty from './ui/empty';
+
+	type NavLinks = {
+		name: string;
+		icon: typeof typeoficon;
+	};
+
+	const navlinks: NavLinks[] = [
+		{
+			name: 'Notifications',
+			icon: Bell
+		},
+		{
+			name: 'Messages',
+			icon: MessageCircle
+		},
+		{
+			name: 'Search',
+			icon: Search
+		}
+	];
+</script>
+
+<div class="mx-auto flex w-[100rem] items-center justify-between gap-4 p-6">
+	<div>
+		<h1
+			class="bg-gradient-to-r from-cyan-400 via-sky-500 to-blue-600 bg-clip-text text-xl font-bold text-transparent"
+		>
+			Welcome Back, Haider!
+		</h1>
+		<p class="text-sm text-muted-foreground">welcome back, we missed you</p>
+	</div>
+	<div class="flex gap-2">
+		{#each navlinks as link}
+			<DropdownMenu.Root>
+				<p class="flex size-9 items-center justify-center rounded-full border bg-muted-foreground">
+					<DropdownMenu.Trigger>
+						<link.icon class="text-background " size="20" />
+					</DropdownMenu.Trigger>
+				</p>
+				<DropdownMenu.Content class="min-h-50 min-w-70 bg-background ">
+					<DropdownMenu.Label>
+						{link.name}
+					</DropdownMenu.Label>
+					<DropdownMenu.Separator />
+					<DropdownMenu.Item>
+						<Empty.Root>
+							<Empty.Media variant="icon">
+								<link.icon size="20" />
+							</Empty.Media>
+							<Empty.Title>
+								No {link.name}
+							</Empty.Title>
+							<Empty.Description>
+								You have no {link.name.toLocaleLowerCase()} at the moment
+							</Empty.Description>
+						</Empty.Root>
+					</DropdownMenu.Item>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+		{/each}
+		<Button variant="default">Create new Order</Button>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Navbar from "$lib/components/navbar.svelte";
+
+</script>
+<div class="absolute main left-60">
+    <Navbar/>
+</div>
